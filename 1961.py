daf0b51684febff8bcd412a45536a2c2f0d17985
Ian <ijae0611@gmail.com>
2025-01-26 19:20:21 +0900

    try

--- 1961.py
+++ 1961.py
@@ -2,6 +2,18 @@
 
 # 제출용 답안
 '''
+N = int(input())
+row_input = []
+matrix_input = []
+
+for i in range(0, N):
+    row_input = list(map(int, input().split()))
+    matrix_input.append(row_input)
 
 '''
+
+N = 3
+a, b, c = 1, 2, 3
+d, e, f = 4, 5, 6
+g, h, i = 7, 8, 9
 
