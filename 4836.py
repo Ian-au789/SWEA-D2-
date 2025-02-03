547ea23e87af2aed5c057343d1c3c155e524324a
Ian <ijae0611@gmail.com>
2025-02-03 17:34:05 +0900

    ready

---
+++ 4836.py
@@ -0,0 +1,17 @@
+# https://swexpertacademy.com/main/learn/course/subjectDetail.do?courseId=AVuPDN86AAXw5UW6&subjectId=AWOVF-WqqecDFAWg#
+
+# 제출용 답안
+'''
+N = int(input())
+row_list = []
+matrix_list = []
+
+for i in range(0, N):
+    row_list = list(map(int, input(),split()))
+    matrix_list.append(row_list)
+
+'''
+
+N = 2
+matrix_input = [[2, 2, 4, 4, 1], [3, 3, 6, 6, 2]]
+
