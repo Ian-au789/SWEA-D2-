64cc29f88da3fecf1e66be33aaa2fc29c4701cba
Ian <ijae0611@gmail.com>
2025-02-03 15:22:02 +0900

    start

---
+++ 2001.py
@@ -0,0 +1,15 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PzOCKAigDFAUq
+
+# 제출용 답안
+'''
+N, M = map(int, input().split())
+
+row_input = []
+matrix_input = []
+
+for i in range(0, N):
+    row_input = list(map(int, input().split()))
+    matrix_input.append(row_input)
+
+
+'''
