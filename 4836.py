d48143d23e4669ac229ee662e415491640b24b69
Ian <ijae0611@gmail.com>
2025-02-03 17:57:07 +0900

    ongoing

--- 4836.py
+++ 4836.py
@@ -1,4 +1,4 @@
-# https://swexpertacademy.com/main/learn/course/subjectDetail.do?courseId=AVuPDN86AAXw5UW6&subjectId=AWOVF-WqqecDFAWg#
+# https://swexpertacademy.com/main/learn/course/subjectDetail.do?courseId=AVuPDN86AAXw5UW6&subjectId=AWOVF-WqqecDFAWg# (색칠하기)
 
 # 제출용 답안
 '''
@@ -11,7 +11,25 @@ for i in range(0, N):
     matrix_list.append(row_list)
 
 '''
+# [(list[0], list[2]), (list[0], list[3]), (list[1], list[2]), (list[1], list[3])]
 
 N = 2
-matrix_input = [[2, 2, 4, 4, 1], [3, 3, 6, 6, 2]]
+matrix_input = [[2, 2, 4, 4, 1], [3, 3, 6, 6, 2]]                  # [[1, 2, 3, 3, 1], [3, 6, 6, 8, 1], [2, 3, 5, 6, 2]]
+
+
+red_area = []
+blue_area = []
+
+
+for list in matrix_input:
+    four_points = []                           
+
+    if list[4] == 1:                                                # list 마지막 값으로 색 판별
+        four_points = [list[0], list[1], list[2], list[3]]
+        red_area.append(four_points)
+
+    else:
+        four_points = [list[0], list[1], list[2], list[3]]
+        blue_area.append(four_points)
+
 
