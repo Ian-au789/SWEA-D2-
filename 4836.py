796bd7bfca810720c96812ef66d634f055dd7a22
Ian <ijae0611@gmail.com>
2025-02-04 17:44:44 +0900

    geometric approach

--- 4836.py
+++ 4836.py
@@ -2,34 +2,107 @@
 
 # 제출용 답안
 '''
-N = int(input())
-row_list = []
-matrix_list = []
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    # ///////////////////////////////////////////////////////////////////////////////////
+    N = int(input())
+    row_input = []
+    matrix_input = []
 
-for i in range(0, N):
-    row_list = list(map(int, input(),split()))
-    matrix_list.append(row_list)
+    for i in range(0, N):
+        row_input = list(map(int, input().split()))
+        matrix_input.append(row_input)
 
+    def find_purple(input_list):
+        red_area = []
+        blue_area = []                                                      # 색깔에 따라 분류
+
+
+        for list in input_list:
+            four_points = []                           
+
+            if list[4] == 1:                                                # list 마지막 값으로 색 판별
+                list.pop()                                                  # 마지막 항목 제거
+                four_points = list
+                red_area.append(four_points)
+
+            else:
+                list.pop()
+                four_points = list
+                blue_area.append(four_points)
+
+
+        coordinate1 = [[0]*10 for _ in range(10)]                           # 빨간색 표시할 좌표평면 
+        coordinate2 = [[0]*10 for _ in range(10)]                           # 파란색 표시할 좌표평면 
+
+        for list in red_area:
+            for x_coor in range(list[0], list[2]+1):
+                for y_coor in range(list[1], list[3]+1):
+                    coordinate1[x_coor][y_coor] = 1                         # 빨간 영역에 1 할당
+
+        for list in blue_area:
+            for x_coor in range(list[0], list[2]+1):
+                for y_coor in range(list[1], list[3]+1):
+                    coordinate2[x_coor][y_coor] = 2                         # 파란 영역에 2 할당
+
+        purple = 0
+
+        for x in range(0, 10):
+            for y in range(0, 10):
+                if coordinate1[x][y] + coordinate2[x][y] == 3:              #빨간색과 파란색이 겹치는 지점 탐색
+                    purple += 1
+
+        return purple
+    
+    
+    result = find_purple(matrix_input)
+    
+    print(f"#{test_case} {result}")
 '''
-# [(list[0], list[2]), (list[0], list[3]), (list[1], list[2]), (list[1], list[3])]
 
 N = 2
-matrix_input = [[2, 2, 4, 4, 1], [3, 3, 6, 6, 2]]                  # [[1, 2, 3, 3, 1], [3, 6, 6, 8, 1], [2, 3, 5, 6, 2]]
+matrix_input = [[1, 2, 3, 3, 1], [3, 6, 6, 8, 1], [2, 3, 5, 6, 2]]
+
+def find_purple(input_list):
+    red_area = []
+    blue_area = []                                                      # 색깔에 따라 분류
 
 
-red_area = []
-blue_area = []
+    for list in input_list:
+        four_points = []                           
+
+        if list[4] == 1:                                                # list 마지막 값으로 색 판별
+            list.pop()                                                  # 마지막 항목 제거
+            four_points = list
+            red_area.append(four_points)
+
+        else:
+            list.pop()
+            four_points = list
+            blue_area.append(four_points)
 
 
-for list in matrix_input:
-    four_points = []                           
+    coordinate1 = [[0]*10 for _ in range(10)]                           # 빨간색 표시할 좌표평면 
+    coordinate2 = [[0]*10 for _ in range(10)]                           # 파란색 표시할 좌표평면 
 
-    if list[4] == 1:                                                # list 마지막 값으로 색 판별
-        four_points = [list[0], list[1], list[2], list[3]]
-        red_area.append(four_points)
+    for list in red_area:
+        for x_coor in range(list[0], list[2]+1):
+            for y_coor in range(list[1], list[3]+1):
+                coordinate1[x_coor][y_coor] = 1                         # 빨간 영역에 1 할당
 
-    else:
-        four_points = [list[0], list[1], list[2], list[3]]
-        blue_area.append(four_points)
+    for list in blue_area:
+        for x_coor in range(list[0], list[2]+1):
+            for y_coor in range(list[1], list[3]+1):
+                coordinate2[x_coor][y_coor] = 2                         # 파란 영역에 2 할당
 
+    purple = 0
 
+    for i in range(0, 10):
+        for j in range(0, 10):
+            if coordinate1[i][j] + coordinate2[i][j] == 3:              #빨간색과 파란색이 겹치는 지점 탐색
+                purple += 1
+    
+    return purple
+
+print(find_purple(matrix_input))
